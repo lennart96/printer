4f94caf37eb5b07e761b7cb4848a0d23f0664a40
basviezee <easyaccesbv@gmail.com>
2014-07-15 12:45:24 +0200

    printercode
    printercode

---
+++ 3D print programma/3D print programma/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class Point3D
+    {
+        public int X;
+        public int Y;
+        public int Z;
+        public void setPoint3D(List<Point3D> a, int x,int y,int z, Point3D p)
+        {
+            p.X = x;
+            p.Y = y;
+            p.Z = z;
+            a.Add(p);
+        }
+    }
+    public class Object3D
+    {
+        private void Print(int x, int y)
+        {
+            Console.WriteLine(x + ", " + y);
+            Console.ReadLine();
+        }
+        public void printObject3D(List<Point3D> a)
+        {
+            while (a.Count > 0)
+            {
+                foreach (Point3D p in a.Where(x=>x.Z!=0))
+                {
+                    Print(p.X, p.Y);
+                    p.Z--;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Point3D> points = new List<Point3D>();
+            Point3D point = new Point3D();
+            Point3D point2 = new Point3D();
+            point.setPoint3D(points, 1, 2, 3, point);
+            point2.setPoint3D(points, 2, 2, 2, point);
+            Object3D obj = new Object3D();
+            obj.printObject3D(points);
+        }
+    }
+}
