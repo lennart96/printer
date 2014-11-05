1e7d104788dd80c079eee7e0f82174fbf3143684
lennart <lennart_96@hotmail.com>
2014-11-05 11:06:29 +0200

    x/y/z axis movement
    not sure if right motors. might have swapped x and y{1,2}
    makes use of undefined _steps_{x,y1,y2} :: int[]

--- Arduino/libraries/Printer/Printer.cpp
+++ Arduino/libraries/Printer/Printer.cpp
@@ -3,6 +3,49 @@
 #include <Motor.h>
 #include <Switch.h>
 
+void reset() {
+    // todo: implement resetting motor, if possible
+    off();
+}
+
+void moveNorth() {
+    m_x.rotateSteps(_steps_x);
+}
+
+void moveSouth() {
+    m_x.rotateSteps(-_steps_x);
+}
+
+void moveEast() {
+    for (int i = 0; i < _steps_y) {
+        m_y1.rotateSteps(1);
+        m_y2.rotateSteps(1);
+    }
+}
+
+void moveWest() {
+    for (int i = 0; i < _steps_y) {
+        m_y1.rotateSteps(-1);
+        m_y2.rotateSteps(-1);
+    }
+}
+
+void on() {
+    m_head.on();
+}
+
+void off() {
+    m_head.off();
+}
+
+void moveUp() {
+    m_z.rotateSteps(_steps_z);
+}
+
+void moveDown() {
+    m_z.rotateSteps(-_steps_z);
+}
+
 void Printer::setMotor(enum motor, int p1, int p2, int p3) {
     Motor motor = Motor(p1, p2, p3, p4);
     switch (motor) {
