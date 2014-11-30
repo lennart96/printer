6d6ab54a29fe6a1471a5c2ab171640575dcf345d
Lennart Landsmeer <lennart_96@hotmail.com>
2014-11-30 12:34:50 +0000

    add motor test

---
+++ printer/src/debug.ino
@@ -0,0 +1,16 @@
+#include <Motor.h>
+
+Motor motor(8, 9, 10, 11);
+
+void setup() {
+    Serial.begin(9600);
+    while(!Serial);
+}
+
+void loop() {
+    if (Serial.available()) {
+        int steps = Serial.parseInt();
+        motor.rotateSteps(steps);
+        Serial.write("Done");
+    }
+}
