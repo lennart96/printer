85f9d03c72f09b3e0b7c80acd3cdcc13a696f4a4
Lennart Landsmeer <lennart_96@hotmail.com>
2014-12-30 11:07:51 +0000

    stepper motor movement

--- path/Command.hs
+++ path/Command.hs
@@ -1,12 +1,28 @@
+{-# LANGUAGE LambdaCase #-}
 module Main(main) where
 
 import Data.Functor
-import Control.Monad (forM_)
+import Text.Read (readMaybe)
+import System.Environment (getArgs)
 
 import Path
 
-main :: IO ()
-main = do
-    arr <- fmap (fmap read . words) . lines <$> getContents
-    forM_ (findPath arr) $ print
+parse :: Int -> Int -> Int -> IO ()
+parse x y z = do
+    arr <- fmap words . lines <$> getContents
+    mapM_ (putStrLn . convert) (findPath $ fmap read <$> arr)
+    where
+        convert (N count) = "y-" ++ show (count*y)
+        convert (S count) = "y"  ++ show (count*y)
+        convert (W count) = "x-" ++ show (count*x)
+        convert (E count) = "x"  ++ show (count*x)
+        convert Up        = "z"  ++ show z
+        convert On        = "+"
+        convert Off       = "-"
 
+main :: IO ()
+main = fmap readMaybe <$> getArgs >>= \case
+    [Just xyz]                  -> parse xyz xyz xyz
+    [Just xy , Just z]          -> parse xy  xy  z
+    [Just x  , Just y, Just z]  -> parse x   y   z
+    _                           -> putStrLn "usage: steps-x [[steps-y] steps-z]"
